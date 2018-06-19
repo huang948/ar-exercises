49d8890478aafaea5e14e2673c865030124f4f68
Jordan Huang <jordanhuang99@gmail.com>
2018-06-19 19:36:40 +0000

    finished exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,14 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+total_revenue = Store.all.sum("annual_revenue")
+puts total_revenue 
+
+all_stores = Store.all
+all_stores.each do |store|
+    puts "#{store.name} #{store.annual_revenue}"
+end
+
+big_stores = Store.where("annual_revenue > ?", 1000000).size
+puts big_stores
