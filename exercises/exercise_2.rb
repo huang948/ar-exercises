74a50fc529084ded8c78e795b9ea9906e7e0655e
Jordan Huang <jordanhuang99@gmail.com>
2018-06-19 18:55:38 +0000

    finished second exercise

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+### Exercise 2: Update the first store
+
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+@store1.name = 'Los Angeles'
+@store1.save
